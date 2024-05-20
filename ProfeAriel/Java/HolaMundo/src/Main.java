9dc8f044fa2c9e67745e6333e66164e5eaea862e
Augusto Vernengo <augustojvl8@gmail.com>
2024-05-20 16:59:03 -0300

    Clases Java

---
+++ ProfeAriel/Java/HolaMundo/src/Main.java
@@ -0,0 +1,33 @@
+public class Main {
+    public static void main(String[] args) {
+        /*
+        System.out.println(" ");
+        System.out.println("Hola Mundo desde Java");
+
+        int miVariable = 10;
+        System.out.println(miVariable);
+        miVariable = 5;
+        System.out.println(miVariable);
+        //tipo string
+        String miVariableCadenna = "bienvenidos";
+        System.out.println(miVariableCadenna);
+        miVariableCadenna = "Sigamos creciendo en programación";
+        System.out.println(miVariableCadenna);
+
+        //reglas para definir una variable en java
+        */
+        var usuario = "Augusto";
+        var titular = "Estudiante";
+        var union = titular + " " + usuario;
+        System.out.println("union = " + union);
+        //con la cadena de usuario se logra un contexto de cadena, concatena con el parentesis y no es una cadena
+        var a = 8;
+        var b = 4;
+        System.out.println(" ");
+        System.out.println(usuario + (a + b));
+        //caracteres especiales con java
+        var nombre = "Julian";
+        System.out.println("Nueva Linea: \n"+nombre);
+
+    }
+}
